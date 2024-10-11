7d35576864625abbe01b06d859a2c962b09d9965
Joshua-SB <129015545+Joshua-SB@users.noreply.github.com>
2024-10-10 22:17:37 -0600

    Delete Kangaroo directory

--- Kangaroo/ArchivosPHP/cerrar_sesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    session_start();
-    session_destroy();
-    header("location: ../Inicio_de_sesion/Inicio.php");
-
-?>
--- Kangaroo/Inicio_de_sesion/script copy.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
-    window.location.href ="../index.html"; // Redirige a index.html
-});
